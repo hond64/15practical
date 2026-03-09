22fd656378096afec782364be4ee002ba36ec95d
hond64 <wentzelmiguel@gmail.com>
2026-03-09 12:15:39 +0000

    Create public class anagrams and public stauic String signature

--- Anagrams.java
+++ Anagrams.java
@@ -1,1 +1,12 @@
+//MIguel Wentzel
+//4478677
+//Practical 5
+import java.util.*;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
+public class anagrams{
+    public static String signature(String word){
+        char[] chars
+    }
+}
